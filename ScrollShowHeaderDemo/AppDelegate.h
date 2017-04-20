fcb33fe2c968b5af1345fb2c44905f7db759e2bb
Cloudox <cloudox@foxmail.com>
2017-04-20 11:00:51 +0800

    Initial Commit

---
+++ ScrollShowHeaderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollShowHeaderDemo
+//
+//  Created by csdc-iMac on 2017/4/20.
+//  Copyright © 2017年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
